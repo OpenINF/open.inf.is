d28bd34e020ca22b8d1754452e56569ef22d3e71
Derek Lewis <dereknongeneric@open.inf.is>
2024-04-21 12:01:12 -0400

    add actual JS verification slocs

--- build/tasks/verify/verify-js.mjs
+++ build/tasks/verify/verify-js.mjs
@@ -1,3 +1,29 @@
-import yarnpkgShell from '@yarnpkg/shell';
+/**
+ * @file Verify JavaScript files are valid & adhere to checkable style guidelines.
+ * @author The OpenINF Authors & Friends
+ * @license MIT OR Apache-2.0 OR BlueOak-1.0.0
+ * @module {ES6Module} build/tasks/verify/verify-js.mjs
+ */
 
-process.exitCode = await yarnpkgShell.execute('npx eslint --ext=.js,.mjs .');
+import { execute } from '@yarnpkg/shell';
+import { globby } from 'globby';
+
+const JSFiles = await globby([
+  '**.mjs',
+  '!_site/',
+  '!node_modules/',
+  '!vendor/',
+]);
+
+let exitCode = 0;
+const scripts = [`biome lint ${JSFiles.join(' ')}`];
+
+for (const element of scripts) {
+  try {
+    exitCode = await execute(element);
+  } catch (p) {
+    exitCode = p.exitCode;
+  }
+
+  if (exitCode !== 0) process.exitCode = exitCode;
+}
