28f95dccb23ab6b6d4e6326ee84e4f4be263da5b
Derek Lewis <dereknongeneric@open.inf.is>
2024-04-21 14:57:16 -0400

    something like this
    then in a follow-up we can put the utils in global scope via loader

--- build/tasks/verify/verify-js.mjs
+++ build/tasks/verify/verify-js.mjs
@@ -2,13 +2,13 @@
  * @file Verify JavaScript files are valid & adhere to checkable style guidelines.
  * @author The OpenINF Authors & Friends
  * @license MIT OR Apache-2.0 OR BlueOak-1.0.0
- * @module {ES6Module} build/tasks/verify/verify-js.mjs
+ * @module {type ES6Module} build/tasks/verify/verify-js
  */
 
 import { execute } from '@yarnpkg/shell';
-import { globby } from 'globby';
+import { filePathGlobs } from '../utils.mjs';
 
-const JSFiles = await globby([
+const JSFiles = await filePathGlobs([
   '**.mjs',
   '!_site/',
   '!node_modules/',
