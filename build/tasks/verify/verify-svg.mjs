189fbc1504bfb437a24f0ec1d4ec48329946d4bd
Derek Lewis <dereknongeneric@open.inf.is>
2024-04-22 01:20:02 -0400

    oops

--- build/tasks/verify/verify-svg.mjs
+++ build/tasks/verify/verify-svg.mjs
@@ -17,7 +17,7 @@ const SVGFiles = await glob([
 
 let exitCode = 0;
 const scripts = [
-  `biome check --apply ${SVGFiles.join(' ')}`,
+  `biome check ${SVGFiles.join(' ')}`,
   `java -jar ${vnu} --svg ${SVGFiles.join(' ')}`,
 ];
 
