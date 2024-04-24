f62319c862e3f627dee9bea24fe58b18c427a02f
Derek Lewis <dereknongeneric@open.inf.is>
2024-04-23 23:31:27 -0400

    🏗✨：create main build task - build portal

---
+++ build/tasks/compile/build-portal.mjs
@@ -0,0 +1,6 @@
+/**
+ * @file Main build task; build portal.
+ * @author The OpenINF Authors & Friends
+ * @license MIT OR Apache-2.0 OR BlueOak-1.0.0
+ * @module {type ES6Module} build/tasks/format/format-svg
+ */
