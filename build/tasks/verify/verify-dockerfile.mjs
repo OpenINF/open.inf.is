3923913225416fabd94d16988eaa66bfaa42d8e6
Derek Lewis <dereknongeneric@open.inf.is>
2024-04-25 01:41:50 -0400

    add dprint dockerfile checks

---
+++ build/tasks/verify/verify-dockerfile.mjs
@@ -0,0 +1,28 @@
+/**
+ * @file Verify Dockerfiles are valid & adhere to checkable style guidelines.
+ * @author The OpenINF Authors & Friends
+ * @license MIT OR Apache-2.0 OR BlueOak-1.0.0
+ * @module {type ES6Module} build/tasks/verify/verify-dockerfile
+ */
+
+import { exec, glob } from '@openinf/portal/build/utils';
+
+const DockerfileFiles = await glob([
+  '**/Dockerfile',
+  '!_site/',
+  '!node_modules/',
+  '!vendor/',
+]);
+
+let exitCode = 0;
+const scripts = [`dprint check ${DockerfileFiles.join(' ')}`];
+
+for (const element of scripts) {
+  try {
+    exitCode = await exec(element);
+  } catch (p) {
+    exitCode = p.exitCode;
+  }
+
+  if (exitCode !== 0) process.exitCode = exitCode;
+}
