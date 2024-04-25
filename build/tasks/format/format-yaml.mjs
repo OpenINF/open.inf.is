e9643e5a9712123dc174067ba965aaf6ea160741
Derek Lewis <dereknongeneric@open.inf.is>
2024-04-25 18:39:36 -0400

    oops

--- build/tasks/format/format-yaml.mjs
+++ build/tasks/format/format-yaml.mjs
@@ -2,7 +2,7 @@
  * @file Format YAML files to adhere to autofixable style guidelines.
  * @author The OpenINF Authors & Friends
  * @license MIT OR Apache-2.0 OR BlueOak-1.0.0
- * @module {type ES6Module} build/tasks/verify/verify-yaml
+ * @module {type ES6Module} build/tasks/format/format-yaml
  */
 
 import { exec, glob } from '@openinf/portal/build/utils';
