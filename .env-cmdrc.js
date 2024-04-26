e065cf1d031777baa5067c3bfd523d1cc4cf9c5c
Derek Lewis <dereknongeneric@open.inf.is>
2024-04-25 20:35:36 -0400

    remove babel config (for now)
    this will likely become more relevant later

--- .env-cmdrc.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable
-   @typescript-eslint/no-var-requires */
-
-/* TODO(DerekNonGeneric): impl @typescript-eslint */
-// require = require('')(module /* , options */);
-module.exports = require('./.config/env-cmd').default;
