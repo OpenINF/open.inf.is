e6ef7ca7c3d0e634d20b201dbd4a5dd4baa42b81
Derek Lewis <dereknongeneric@open.inf.is>
2024-04-28 02:01:29 -0400

    set preferred project constants module location

---
+++ build/shared/constants.mjs
@@ -0,0 +1,12 @@
+/**
+ * @file Constants that are used throughout the project.
+ * @author The OpenINF Authors & Friends
+ * @license MIT OR Apache-2.0 OR BlueOak-1.0.0
+ * @module build/shared/constants
+ */
+
+const PATHS = {};
+
+export default {
+  PATHS
+}
